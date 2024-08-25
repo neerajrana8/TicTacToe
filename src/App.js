9e367f869ea7bec70d03c16f22fb7dcc85d6f7c1
Neeraj Rana <neeraj@getcovered.io>
2024-08-25 20:25:26 +0530

    game

--- src/App.js
+++ src/App.js
@@ -63,9 +63,9 @@ function calculateWinner(squares) {
         [2, 4, 6]
     ];
 
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    for (let  i=0; i < lines.length; i++ ){
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
             return squares[a];
         }
     }
